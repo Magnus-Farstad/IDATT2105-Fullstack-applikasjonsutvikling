5d61ae5035fd6bcb32ea944f7de484b568525743
unknown <magnusrf@stud.ntnu.no>
2022-02-16 13:53:54 +0100

    Imported calculator and log from Oving_2

---
+++ oving_3/src/components/Log.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="scrollBox" class="log">
+    <ul id="arrayUnorderedList">
+      <li v-for="(equation, index) in equations" :key="index">
+        {{ equation }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Log",
+  props: {
+    equations: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+#scrollBox {
+  overflow: scroll;
+  border: solid black;
+  margin-top: 10px;
+  height: 400px;
+  background-color: rgba(255, 165, 0, 0.14);
+}
+
+.log {
+  grid-area: log;
+}
+</style>
--- oving_3/src/views/Home.vue
+++ oving_3/src/views/Home.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="headerContainer">
+      <h1 class="header">Calculator</h1>
+      <div class="text">This is a calculator</div>
+    </div>
+    <div id="calculatorContainer">
+      <calculator @add-equations="addEquation"></calculator>
+      <log :equations="equations"></log>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
+import Calculator from "@/components/Calculator.vue";
+import Log from "@/components/Log.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Calculator,
+    Log,
+  },
+  data() {
+    return {
+      equations: [],
+    };
+  },
+  methods: {
+    addEquation(equation) {
+      this.equations.push(equation);
+      //console.log(equation);
+      for (let i = 0; i < this.equations.length; i++) {
+        console.log(this.equations[i]);
+      }
+    },
   },
 };
 </script>
+<style scoped>
+body {
+  align-items: center;
+}
+
+#calculatorContainer {
+  display: grid;
+  grid-template-areas:
+    "display display display display"
+    "c ans del plus"
+    "one two three subtract"
+    "four five six multiply"
+    "seven eight nine divide"
+    "empty zero point equals"
+    "log log log log";
+  background-color: #eee;
+  padding: 7px;
+  height: 100%;
+  margin: 0 auto;
+  text-align: center;
+  width: 40%;
+  font-size: 40px;
+  grid-auto-rows: minmax(50px, auto);
+  box-shadow: 0 0 16px #363535;
+}
+
+.headerContainer {
+  margin: 40px auto;
+  text-align: center;
+}
+
+
+.header {
+  font-size: 50px;
+  font-weight: 900;
+}
+
+.text {
+}
+
+
+
+</style>
