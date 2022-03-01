436f0f17373065400ebff92cbf8ab451d20fb9ea
unknown <magnusrf@stud.ntnu.no>
2022-03-01 10:44:18 +0100

    oving4: Initial commit

---
+++ oving4/src/test/java/magnus/ntnu/no/oving4/Oving4ApplicationTests.java
@@ -0,0 +1,13 @@
+package magnus.ntnu.no.oving4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oving4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
